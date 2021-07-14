f70960eeeaf805804aee32e9ed22caa9fd31dd72
srv-shivam <shivamsrivastava222001@gmail.com>
2021-07-15 01:40:03 +0530

    Added Splash screen activity, and Profile fragment UI work.

---
+++ app/src/main/java/com/example/tajhotel/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.tajhotel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT=3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+        bottomNavButton();
+    }
+
+    public void bottomNavButton() {
+        getWindow().setNavigationBarColor(getResources().getColor(R.color.grey));
+    }
+}
